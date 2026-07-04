ba8437f974c6cdf27823626e3173aca7ac1139b0
Mayabi-Ivan <mayabivan@gmail.com>
2026-07-04 12:22:11 +0300

    fix: Slow database responses
    Description:
    - Package database queries on the app itself

--- config/database.php
+++ config/database.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Str;
+use PDO;
 use Pdo\Mysql;
 
 return [
@@ -97,6 +98,9 @@ return [
             'prefix_indexes' => true,
             'search_path' => 'public',
             'sslmode' => env('DB_SSLMODE', 'prefer'),
+            'options'=> [
+                PDO::ATTR_EMULATE_PREPARES => true,
+            ],
         ],
 
         'sqlsrv' => [
