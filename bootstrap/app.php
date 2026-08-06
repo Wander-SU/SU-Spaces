b1af2c123bb3fe54b3e53d71940de1f2649e55a5
Mayabi-Ivan <mayabivan@gmail.com>
2026-08-06 18:57:05 +0300

    fix: Broken css links on production
    Description:
    - Forced the app to use https links in prod
    - Forced the app to trust proxies from all ip addresses

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // Trust Render's proxies
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
